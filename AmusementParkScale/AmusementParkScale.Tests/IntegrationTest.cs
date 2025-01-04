217b241f018830d23258de3f219552d8b513c214
BergenChris <bergen.chris@live.be>
2025-01-04 20:17:34 +0100

    integration testing getting person from API

---
+++ AmusementParkScale/AmusementParkScale.Tests/IntegrationTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmusementParkScale.Tests
+{
+    public class IntegrationTest
+    {
+
+        private const double weight = 63.16;
+        private const string MockoonUrl = "https://dummyjson.com/users/1";
+
+        [Test]
+        public void WeighFromAPI() 
+        {
+            var person = new Person() { Url = MockoonUrl };
+
+            var result = person.Weigh();
+
+            Assert.That(result, Is.EqualTo(weight));
+        }
+    }
+}
