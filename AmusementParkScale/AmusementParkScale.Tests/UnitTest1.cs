11352f1a02182610587358c12de095fa8335c3ce
BergenChris <bergen.chris@live.be>
2025-01-04 14:56:15 +0100

    test 1.1 fail

---
+++ AmusementParkScale/AmusementParkScale.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AmusementParkScale.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1_1_IsThereAScreen()
+        {
+            var newScreen = new Screen();
+        }
+    }
+}
